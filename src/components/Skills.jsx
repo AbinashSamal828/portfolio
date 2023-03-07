7098de0ee65999b98e41f77b1f7c020448780b47
Abinash <abinashsamal18@gmail.com>
2023-03-07 23:29:44 +0530

    skill section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import TechCard from './TechCard'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import javascript from '../assets/javascript.png'
+import node from '../assets/node.png'
+import firebase from '../assets/firebase.png'
+import cpp from '../assets/cpp.png'
+import mongo from '../assets/mongo.png'
+import github from '../assets/github.png'
+
+
+const Skills = () => {
+  return (
+    <div className='w-full h-auto bg-black text-white pb-[100px] pt-[100px]'>
+        <div className='w-full text-center text-5xl font-bold mb-10 text-cyan-500'>
+            <span className='border-b-4'>Skills</span>
+        </div>
+        <div className='max-w-[60%] w-[60%] justify-center flex flex-row mx-auto flex-wrap'>
+            <TechCard img={html} name="HTML"/>
+            <TechCard img={css} name="CSS"/>
+            <TechCard img={javascript} name="JAVASCRIPT"/>
+            <TechCard img={node} name="NODE"/>
+            <TechCard img={firebase} name="FIREBASE"/>
+            <TechCard img={cpp} name="C++"/>
+            <TechCard img={mongo} name="MONGO DB"/>
+            <TechCard img={github} name="GITHUB"/>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
---
+++ src/components/TechCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TechCard = (props) => {
+    const img=props.img;
+    const name=props.name;
+  return (
+    <div className='w-54 flex flex-col text-center  p-3 m-3 '>
+        <div className='w-36 flex justify-center m-2'>
+            <img className='w-32 md:w-24' src={img} alt={name} />
+        </div>
+        <div>
+            {name}
+        </div>
+    </div>
+  )
+}
+
+export default TechCard
