d7c2120e7f475ee180641ca876ce746236c736b0
Abinash <abinashsamal18@gmail.com>
2024-08-15 01:09:08 +0530

    added new project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,20 +9,20 @@ const About = () => {
         </div>
         <p className="mt-4 sm:text-xl">
           My name is Abinash Samal and I am a full stack web developer with a
-          passion for problem-solving. I am currently pursuing a Bachelor of
-          Technology in Computer Science and Engineering and am in my third year
-          of studies. My interest in web development began during my first year
-          of college, and since then, I have been continuously working to
-          improve my skills in this field. I have experience working with
-          various web development technologies such as HTML, CSS, JavaScript,
-          React, Node.js, and more. Apart from web development, I also enjoy
-          practicing Data Structures and Algorithms (DSA) problem-solving, which
-          has helped me develop strong analytical and problem-solving skills.
-          Through this website, I aim to showcase my work as a full stack web
-          developer and share my knowledge and experiences in web development
-          and DSA problem-solving. Please feel free to explore my projects, and don't hesitate to reach out to me for any inquiries or
-          collaborations. Thank you for visiting my website, and I hope you find
-          it informative and helpful!
+          passion for problem-solving. I have graduated with a Bachelor of
+          Technology in Computer Science and Engineering. My interest in web
+          development began during my first year of college, and since then, I
+          have been continuously working to improve my skills in this field. I
+          have experience working with various web development technologies such
+          as HTML, CSS, JavaScript, React, Node.js, and more. Apart from web
+          development, I also enjoy practicing Data Structures and Algorithms
+          (DSA) problem-solving, which has helped me develop strong analytical
+          and problem-solving skills. Through this website, I aim to showcase my
+          work as a full stack web developer and share my knowledge and
+          experiences in web development and DSA problem-solving. Please feel
+          free to explore my projects, and don't hesitate to reach out to me for
+          any inquiries or collaborations. Thank you for visiting my website,
+          and I hope you find it informative and helpful!
         </p>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,55 +1,63 @@
-import React from 'react'
-import ProjectCard from './ProjectCard';
-import ping from '../assets/ping.png'
-import qlib from '../assets/qlib.png'
-import extracker from '../assets/extracker.png'
-import fosc from '../assets/fosc.png'
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import ping from "../assets/ping.png";
+import qlib from "../assets/qlib.png";
+import extracker from "../assets/extracker.png";
+import fosc from "../assets/fosc.png";
+import todo from "../assets/todo.png";
 const projects = [
-    {
-      title: 'Ping',
-      description: 'A fully functional chat app made with react.js and socket.io',
-      image: ping,
-      url: 'https://heypingme.netlify.app/',
-      gurl: 'https://github.com/AbinashSamal828/ping'
-    },
-    {
-      title: 'Quote Library',
-      description: 'A web app made with react.js and firebase where users can post quotes and compliment other qoutes.',
-      image: qlib,
-      url: 'https://quote-app-a5c53.web.app/quotes',
-      gurl: 'https://github.com/AbinashSamal828/Quote-Library'
-    },
-    {
-        title: 'Expense Tracker',
-        description: 'web app to keep the record of the monthly expenses',
-        image: extracker,
-        url: 'https://trackexpense123.netlify.app/',
-        gurl: 'https://github.com/AbinashSamal828/ExpenseTracker'
-
-      }
-      ,{
-        title: 'FoSC',
-        description: 'Project on food shortage assumption.Which predicts the chances of food shortage in different states.',
-        image: fosc,
-        gurl: 'https://github.com/AbinashSamal828/FoSC'
-      }
-  ];
+  {
+    title: "Ping",
+    description: "A fully functional chat app made with react.js and socket.io",
+    image: ping,
+    url: "https://heypingme.netlify.app/",
+    gurl: "https://github.com/AbinashSamal828/ping",
+  },
+  {
+    title: "Todo App",
+    description: "A todolist app with multiple functionalitis",
+    image: todo,
+    gurl: "https://github.com/AbinashSamal828/todolist-app",
+  },
+  {
+    title: "Quote Library",
+    description:
+      "A web app made with react.js and firebase where users can post quotes and compliment other qoutes.",
+    image: qlib,
+    url: "https://quote-app-a5c53.web.app/quotes",
+    gurl: "https://github.com/AbinashSamal828/Quote-Library",
+  },
+  {
+    title: "Expense Tracker",
+    description: "web app to keep the record of the monthly expenses",
+    image: extracker,
+    url: "https://trackexpense123.netlify.app/",
+    gurl: "https://github.com/AbinashSamal828/ExpenseTracker",
+  },
+  {
+    title: "FoSC",
+    description:
+      "Project on food shortage assumption.Which predicts the chances of food shortage in different states.",
+    image: fosc,
+    gurl: "https://github.com/AbinashSamal828/FoSC",
+  },
+];
 
 const Projects = () => {
   return (
     <div className="p-8 px-14 bg-black ">
       <div className="text-3xl font-bold sm:text-6xl mt-2 text-cyan-500 mb-10 text-center">
-          <span className="border-b-4">My Projects</span>
-        </div>
+        <span className="border-b-4">My Projects</span>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projects.map(project => (
+        {projects.map((project) => (
           <div key={project.title}>
             <ProjectCard project={project} />
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
